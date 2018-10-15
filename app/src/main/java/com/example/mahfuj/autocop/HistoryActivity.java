48dc02030470a92969e0b576e644f21f958c9e26
mahfuj7225 <43011519+mahfuj7225@users.noreply.github.com>
2018-10-16 04:27:46 +0600

    Update HistoryActivity.java

--- app/src/main/java/com/example/mahfuj/autocop/HistoryActivity.java
+++ app/src/main/java/com/example/mahfuj/autocop/HistoryActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class HistoryActivity extends AppCompatActivity {
+    private FirebaseAuth = mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
